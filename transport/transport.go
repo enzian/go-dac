4a653d5e8ec60a7d5ffd61341aaf1e56c83ca7af
Michael (Enzian) Ingold <github@michaelingold.ch>
2016-05-12 17:57:14 +0200

    added preliminary implementation for receive pack

--- transport/transport.go
+++ transport/transport.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"github.com/go-msf/go-dac"
 )
@@ -10,6 +11,12 @@ import (
 type ProtoLine struct {
 	Command string
 	Content []byte
+}
+
+type RefUpdate struct {
+	From    dac.ObjectID
+	To      dac.ObjectID
+	RefName string
 }
 
 var (
@@ -28,4 +35,52 @@ func RefDiscovery(graph *dac.Graph, sends chan<- ProtoLine) {
 	sends <- ProtoLine{}
 
 	close(sends)
+}
+
+func ReceiveObjects(graph *dac.Graph, recvLines <-chan ProtoLine, sends chan<- ProtoLine) []RefUpdate {
+	RefDiscovery(graph, sends)
+	var updates = readReferenceUpdates(recvLines)
+	return updates
+}
+
+func readReferenceUpdates(recvLines <-chan ProtoLine) []RefUpdate {
+	var updates = []RefUpdate{}
+
+	for line := range recvLines {
+		switch line.Command {
+		case PkgLineFlush.Command:
+			return updates
+		default:
+			var from, err = hex.DecodeString(line.Command)
+			var fromID = dac.ObjectID{}
+
+			if err != nil {
+				continue
+			}
+			copy(fromID[:], from)
+
+			var content = string(line.Content[:len(line.Content)])
+
+			var segments = strings.Split(content, " ")
+			to, err := hex.DecodeString(segments[0])
+
+			if err != nil {
+				continue
+			}
+
+			var toID = dac.ObjectID{}
+			copy(toID[:], to)
+
+			var name = segments[1]
+
+			var update = RefUpdate{
+				From:    fromID,
+				To:      toID,
+				RefName: name}
+
+			updates = append(updates, update)
+		}
+	}
+
+	return updates
 }
--- transport/transport_test.go
+++ transport/transport_test.go
@@ -47,3 +47,41 @@ func TestReferenceDiscovery(t *testing.T) {
 	assert.Equal(t, PkgLineFlush.Command, flush.Command, "The line should be a pkg-flush (0000) but was not")
 	assert.Equal(t, PkgLineFlush.Content, flush.Content[:], "The content of the pkg-flush should have been empty but was not.")
 }
+
+func TestSendPackage(t *testing.T) {
+	// Arrange
+	var from = "4e0ec4f2bddac0eefa18b8560edf158be96fcaccf6644f1d6a41632c8e31e3cf4e0ec4f2bddac0eefa18b8560edf158be96fcaccf6644f1d6a41632c8e31e3cf"
+	var to = "644f1d6a41632c8e31e3cf4e0ec4f2bdd8be96fcaccf658be96fcaccf6644f1d6a41632c8e31e3cfac0eefa18b8560edf14e0ec4f2bddac0eefa18b8560edf15"
+	var name = "refs/heads/master"
+	var command = from
+	var content = []byte(fmt.Sprintf("%s %s", to, name))
+
+	var sendChan = make(chan ProtoLine, 2)
+	var rcvChan = make(chan ProtoLine, 1)
+
+	// Set up the graph
+	var fakeAdapter = memory.NewMemoryAdapter()
+	var graph, _ = dac.NewDACGraph(fakeAdapter, fakeAdapter)
+
+	// Act
+	sendChan <- ProtoLine{Command: command, Content: content}
+	sendChan <- PkgLineFlush
+	close(sendChan)
+
+	var ret []RefUpdate
+
+	var unlock = make(chan bool, 1)
+
+	go func() {
+		ret = ReceiveObjects(graph, sendChan, rcvChan)
+		unlock <- true
+	}()
+
+	<-unlock
+
+	// Assert
+
+	assert.Equal(t, 1, len(ret), "Expected to receive 1 update line but did not")
+	assert.Equal(t, from, hex.EncodeToString(ret[0].From[:]), "To points to a false ID")
+	assert.Equal(t, to, hex.EncodeToString(ret[0].To[:]), "To points to a false ID")
+}
